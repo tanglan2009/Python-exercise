3d9360084e1d50dcb63dfda2277d91c49585b06a
tanglan2009 <tanglan2009@gmail.com>
2015-09-28 09:07:19 -0700

    modify readme file

--- isEven.py
+++ isEven.py
@@ -1,12 +1,23 @@
-def isEven(n):
 
-    if n == 0:
+
+
+#Write a short Python function, is even(k), that takes an integer value and returns
+# True if k is even, and False otherwise. However, your function cannot
+# use the multiplication, modulo, or division operators.
+
+def isEven(k):
+    '''
+    k: an integer value.
+    return: True if k is even and False otherwise.
+    '''
+
+    if k == 0:
         return True
-    if n == 1:
+    if k == 1:
         return False
 
-    if n > 0:
-        return isEven(n-2)
+    if k > 0:
+        return isEven(k-2)
 
 
 print isEven(24)
