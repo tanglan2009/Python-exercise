a54ff362b8e4102e6d2eaadd8abe96171981ee87
tanglan2009 <tanglan2009@gmail.com>
2015-09-30 14:47:23 -0700

    sumSquares.py

--- sumSquares.py
+++ sumSquares.py
@@ -15,7 +15,7 @@ def sumSquares(n):
     return sum
 
 
-print sumSquares(12)
+# print sumSquares(12)
 
 # Give a single command that computes the sum from above, relying on
 # Python's comprehension syntax and the build-in sum function.
@@ -25,4 +25,33 @@ def sumSquares2(n):
     total = sum(i*i for i in range(1, n))
     return total
 
-print sumSquares2(12)
+# print sumSquares2(12)
+
+# Write a short Python function that takes a positive integer n
+# and returns the sum of the squares of all the odd positive
+# integers smaller than n.
+
+def sumOddSquares(n):
+    sum = 0
+    for i in range(n):
+        if i%2 != 0:
+            sum += i * i
+    return sum
+
+print sumOddSquares(21)
+
+
+def sumOddSquares2(n):
+    oddTotal = sum(i*i for i in range(1, n) if i%2 != 0)
+    return oddTotal
+print sumOddSquares2(21)
+
+
+
+
+
+
+
+
+
+
